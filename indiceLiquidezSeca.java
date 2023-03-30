40c0d3d5ef647a9ae7d915d239db34cb4cbddd63
jcamposmelo <jcamposmelo@yahoo.com.br>
2023-03-30 15:37:11 -0300

    Como calcular o Índice de Liquidez Seca?

---
+++ indiceLiquidezSeca.java
@@ -0,0 +1,15 @@
+public class indiceLiquidezSeca {
+	public static void main(String[] args) {
+		double ativoCirculante = 21190000;
+		double passivoCirculante = 17503000;
+		double estoques = 518000;
+
+		double liquidezSeca;
+
+		liquidezSeca = (ativoCirculante - estoques)	/ passivoCirculante;
+
+		System.out.println("O índice calculado em liquidez Seca é?  " + 
+				liquidezSeca);
+	}
+
+}
